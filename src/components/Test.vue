1bd688518a060e360f171c28802af951a3969008
Hitagi7 <ninkie123@gmail.com>
2025-05-09 18:11:02 +0800

    cleaned files & installed tailwindcss

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello from Test.vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
