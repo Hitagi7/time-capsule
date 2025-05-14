8466ca1b2a4881baade50b26dbaa5f15eaf70397
Hitagi7 <20800091@usc.edu.ph>
2025-05-14 23:35:51 +0800

    added animations for login and signup overlay

--- src/components/Landing/LoginOverlay.vue
+++ src/components/Landing/LoginOverlay.vue
@@ -18,69 +18,101 @@ const handleLogin = () => {
 </script>
 
 <template>
-  <div
-    class="fixed h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-    v-if="showOverlay"
-  >
-    <div class="bg-[#393939] rounded-xl overflow-hidden px-16 py-12">
-      <div class="mb-8">
-        <h2 class="w-96 text-white text-3xl font-bold font-['Inter']">Log in to TimeLock</h2>
-      </div>
-      <form @submit.prevent="handleLogin">
-        <div class="flex flex-col gap-6">
-          <div class="flex flex-col gap-3">
-            <label class="font-semibold font-['Inter']">Email</label>
-            <div>
-              <img class="absolute ml-4 mt-2.5" :src="mailIcon" alt="Mail Icon" />
-              <input
-                type="email"
-                v-model="email"
-                class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
-                placeholder="john_doe@email.com"
-                required
-              />
-            </div>
-          </div>
-          <div class="flex flex-col gap-3">
-            <label class="font-semibold font-['Inter']">Password</label>
-            <div>
-              <img class="absolute ml-4 mt-2.5" :src="passwordIcon" alt="Password Icon" />
-              <input
-                type="password"
-                v-model="password"
-                class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
-                placeholder="Should be 8 characters and above"
-                required
-              />
-            </div>
-          </div>
+  <transition name="fade-slide">
+    <div
+      class="fixed h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      v-if="showOverlay"
+    >
+      <div class="bg-[#393939] rounded-xl overflow-hidden px-16 py-12">
+        <div class="mb-8">
+          <h2 class="w-96 text-white text-3xl font-bold font-['Inter']">Log in to TimeLock</h2>
         </div>
-        <div class="flex flex-col gap-6 mt-8">
-          <button
-            type="submit"
-            class="shadow-xl py-6 h-20 bg-black rounded-xl inline-flex justify-center items-center text-white text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
-          >
-            Login
-          </button>
-          <button
-            @click="props.closeLogin"
-            class="shadow-xl py-6 h-12 bg-white rounded-xl inline-flex justify-center items-center text-black text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
-          >
-            Cancel
-          </button>
-
-          <p
-            class="bg-inherit text-white text-sm font-normal font-['Inter'] text-center justify-start"
-          >
-            Need an account?
-            <span
-              class="text-indigo-400 text-sm font-normal font-['Inter'] underline hover:cursor-pointer hover:text-indigo-500"
-              @click="props.openSignup"
-              >Create an account.</span
+        <form @submit.prevent="handleLogin">
+          <div class="flex flex-col gap-6">
+            <div class="flex flex-col gap-3">
+              <label class="font-semibold font-['Inter']">Email</label>
+              <div>
+                <img class="absolute ml-4 mt-2.5" :src="mailIcon" alt="Mail Icon" />
+                <input
+                  type="email"
+                  v-model="email"
+                  class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
+                  placeholder="john_doe@email.com"
+                  required
+                />
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <label class="font-semibold font-['Inter']">Password</label>
+              <div>
+                <img class="absolute ml-4 mt-2.5" :src="passwordIcon" alt="Password Icon" />
+                <input
+                  type="password"
+                  v-model="password"
+                  class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
+                  placeholder="Should be 8 characters and above"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-6 mt-8">
+            <button
+              type="submit"
+              class="shadow-xl py-6 h-20 bg-black rounded-xl inline-flex justify-center items-center text-white text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
             >
-          </p>
-        </div>
-      </form>
+              Login
+            </button>
+            <button
+              @click="props.closeLogin"
+              type="button"
+              class="shadow-xl py-6 h-12 bg-white rounded-xl inline-flex justify-center items-center text-black text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
+            >
+              Cancel
+            </button>
+
+            <p
+              class="bg-inherit text-white text-sm font-normal font-['Inter'] text-center justify-start"
+            >
+              Need an account?
+              <span
+                class="text-indigo-400 text-sm font-normal font-['Inter'] underline hover:cursor-pointer hover:text-indigo-500"
+                @click="props.openSignup"
+                >Create an account.</span
+              >
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
+
+<style>
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
+}
+
+.fade-slide-enter-from {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+
+.fade-slide-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.fade-slide-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
--- src/components/Landing/SignupOverlay.vue
+++ src/components/Landing/SignupOverlay.vue
@@ -24,82 +24,85 @@ const handleSignup = () => {
 </script>
 
 <template>
-  <div
-    class="fixed h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-    v-if="showOverlay"
-  >
-    <div class="bg-[#393939] rounded-xl overflow-hidden px-16 py-12">
-      <div class="flex justify-between mb-8">
-        <h2 class="w-96 text-white text-3xl font-bold font-['Inter']">Sign up to TimeLock</h2>
-      </div>
-      <form @submit.prevent="handleSignup">
-        <div class="flex flex-col gap-6">
-          <div class="flex flex-col gap-3">
-            <label class="font-semibold font-['Inter']">Email</label>
-            <div>
-              <img class="absolute ml-4 mt-2.5" :src="mailIcon" alt="Mail Icon" />
-              <input
-                type="email"
-                v-model="email"
-                class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
-                placeholder="john_doe@email.com"
-                required
-              />
-            </div>
-          </div>
-          <div class="flex flex-col gap-3">
-            <label class="font-semibold font-['Inter']">Password</label>
-            <div>
-              <img class="absolute ml-4 mt-2.5" :src="passwordIcon" alt="Password Icon" />
-              <input
-                type="password"
-                v-model="password"
-                class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
-                placeholder="Should be 8 characters and above"
-                required
-              />
-            </div>
-          </div>
-          <div class="flex flex-col gap-3">
-            <label class="font-semibold font-['Inter']">Confirm Password</label>
-            <div>
-              <img class="absolute ml-4 mt-2.5" :src="passwordIcon" alt="Password Icon" />
-              <input
-                type="password"
-                v-model="confirmPassword"
-                class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
-                placeholder="Should be 8 characters and above"
-                required
-              />
-            </div>
-          </div>
+  <transition name="fade-slide">
+    <div
+      class="fixed h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      v-if="showOverlay"
+    >
+      <div class="bg-[#393939] rounded-xl overflow-hidden px-16 py-12">
+        <div class="flex justify-between mb-8">
+          <h2 class="w-96 text-white text-3xl font-bold font-['Inter']">Sign up to TimeLock</h2>
         </div>
-        <div class="flex flex-col gap-6 mt-8">
-          <button
-            type="submit"
-            class="shadow-xl py-6 h-20 bg-black rounded-xl inline-flex justify-center items-center text-white text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
-          >
-            Create Account
-          </button>
-          <button
-            @click="props.closeSignup"
-            class="shadow-xl py-6 h-12 bg-white rounded-xl inline-flex justify-center items-center text-black text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
-          >
-            Cancel
-          </button>
-
-          <p
-            class="bg-inherit text-white text-sm font-normal font-['Inter'] text-center justify-start"
-          >
-            Already have an account?
-            <span
-              class="text-indigo-400 text-sm font-normal font-['Inter'] underline hover:cursor-pointer hover:text-indigo-500"
-              @click="props.openLogin"
-              >Log in.</span
+        <form @submit.prevent="handleSignup">
+          <div class="flex flex-col gap-6">
+            <div class="flex flex-col gap-3">
+              <label class="font-semibold font-['Inter']">Email</label>
+              <div>
+                <img class="absolute ml-4 mt-2.5" :src="mailIcon" alt="Mail Icon" />
+                <input
+                  type="email"
+                  v-model="email"
+                  class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
+                  placeholder="john_doe@email.com"
+                  required
+                />
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <label class="font-semibold font-['Inter']">Password</label>
+              <div>
+                <img class="absolute ml-4 mt-2.5" :src="passwordIcon" alt="Password Icon" />
+                <input
+                  type="password"
+                  v-model="password"
+                  class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
+                  placeholder="Should be 8 characters and above"
+                  required
+                />
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <label class="font-semibold font-['Inter']">Confirm Password</label>
+              <div>
+                <img class="absolute ml-4 mt-2.5" :src="passwordIcon" alt="Password Icon" />
+                <input
+                  type="password"
+                  v-model="confirmPassword"
+                  class="w-full px-11 py-2 border rounded-lg text-black outline outline-2 outline-offset-[-1px] outline-slate-300"
+                  placeholder="Should be 8 characters and above"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-6 mt-8">
+            <button
+              type="submit"
+              class="shadow-xl py-6 h-20 bg-black rounded-xl inline-flex justify-center items-center text-white text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
             >
-          </p>
-        </div>
-      </form>
+              Create Account
+            </button>
+            <button
+              @click="props.closeSignup"
+              type="button"
+              class="shadow-xl py-6 h-12 bg-white rounded-xl inline-flex justify-center items-center text-black text-lg font-bold font-['Inter'] hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out"
+            >
+              Cancel
+            </button>
+
+            <p
+              class="bg-inherit text-white text-sm font-normal font-['Inter'] text-center justify-start"
+            >
+              Already have an account?
+              <span
+                class="text-indigo-400 text-sm font-normal font-['Inter'] underline hover:cursor-pointer hover:text-indigo-500"
+                @click="props.openLogin"
+                >Log in.</span
+              >
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
